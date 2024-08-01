e3490bb1caf6cec2512978d1470b6c5d14db2c6f
Mike OneFive <johndoe@example.com>
2024-08-01 16:39:14 +0200

    cat entity updated with additional properties

--- version1.0/src/main/java/io/everyonecodes/backend/version1/data/Cat.java
+++ version1.0/src/main/java/io/everyonecodes/backend/version1/data/Cat.java
@@ -3,6 +3,8 @@ package io.everyonecodes.backend.version1.data;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "cats")
 public class Cat {
@@ -15,15 +17,31 @@ public class Cat {
     private String name;
 
     @Column()
+    private int age;
+
+    @Column()
     private String description;
 
     @Column()
     private String location;
 
+    @Column()
+    private boolean isUpForAdoption;
+
+    @Column()
+    private boolean isNeutered;
+
+    @Column()
+    private boolean isOutdoorCat;
+
+    @Column()
+    @ElementCollection(fetch = FetchType.EAGER)
+    List<String> healthAttributes;
+
+
     @ManyToOne()
     @JoinColumn(name = "human_id")
     private Human human;
-
 
 
     public Cat(String name, Human human) {
@@ -31,6 +49,27 @@ public class Cat {
         this.human = human;
     }
 
+    public Cat(Long id,
+               String name,
+               int age,
+               String description,
+               String location,
+               boolean isUpForAdoption,
+               boolean isNeutered,
+               boolean isOutdoorCat,
+               List<String> healthAttributes,
+               Human human) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.description = description;
+        this.location = location;
+        this.isUpForAdoption = isUpForAdoption;
+        this.isNeutered = isNeutered;
+        this.isOutdoorCat = isOutdoorCat;
+        this.healthAttributes = healthAttributes;
+        this.human = human;
+    }
 
     public Cat() {
     }
@@ -51,6 +90,14 @@ public class Cat {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getDescription() {
         return description;
     }
@@ -65,6 +112,38 @@ public class Cat {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public boolean isUpForAdoption() {
+        return isUpForAdoption;
+    }
+
+    public void setUpForAdoption(boolean upForAdoption) {
+        isUpForAdoption = upForAdoption;
+    }
+
+    public boolean isNeutered() {
+        return isNeutered;
+    }
+
+    public void setNeutered(boolean neutered) {
+        isNeutered = neutered;
+    }
+
+    public boolean isOutdoorCat() {
+        return isOutdoorCat;
+    }
+
+    public void setOutdoorCat(boolean outdoorCat) {
+        isOutdoorCat = outdoorCat;
+    }
+
+    public List<String> getHealthAttributes() {
+        return healthAttributes;
+    }
+
+    public void setHealthAttributes(List<String> healthAttributes) {
+        this.healthAttributes = healthAttributes;
     }
 
     public Human getHuman() {
